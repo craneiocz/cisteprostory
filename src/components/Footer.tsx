4b5f24a4cf411c3aa87bd80ff7c14724d3944175
UFOGA <36482397+UFOGA@users.noreply.github.com>
2025-10-23 09:25:50 +0200

    Update Footer.tsx
    uprava tel. čísla

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-semibold text-white mb-4">Kontakt</h3>
             <ul className="space-y-2">
-              <li className="text-white/80">+420 123 456 789</li>
+              <li className="text-white/80">+420 987 654 321</li>
               <li className="text-white/80">info@cisteprostory.eu</li>
               <li className="text-white/80">Česká republika</li>
             </ul>
